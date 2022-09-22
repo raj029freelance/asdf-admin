4e4405425234cb2edccba428640ad8f4aac8e2b3
saranrajfl <saranraj.freelance@gmail.com>
2022-09-22 23:48:28 +0530

    adds delete button

--- src/components/smart/scrappedOrgs/scrappedOrgs.jsx
+++ src/components/smart/scrappedOrgs/scrappedOrgs.jsx
@@ -4,9 +4,48 @@ import { ContentState, EditorState } from "draft-js";
 import htmlToDraft from "html-to-draftjs";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import {
+  useAddOrganizationsMutation,
+  useDeleteOrganizationMutation,
+  useGetPaginatedOrganizationsQuery,
+  useEditOrganizationMutation,
+  useLazySearchByCompanyNameQuery,
+} from "services/organization";
+import { useSelector } from "react-redux";
+import ConfirmationModal from "../confirmationModal/confirmationModal";
 
 const ExtrernallyAddedOrgs = () => {
   const [blogsList, setBlogsList] = useState([]);
+  const [companyToBeDeleted, setCompanyToBeDeleted] = useState(null);
+  const [isConfirmationModalVisible, setConfirmationModal] = useState(false);
+  const [deleteOrganization] = useDeleteOrganizationMutation();
+  const { role } = useSelector((state) => state.user.user);
+
+  const openConfirmationModal = (companyId) => {
+    setCompanyToBeDeleted(companyId);
+    setConfirmationModal(true);
+  };
+
+  const deleteHandler = (_id) => {
+    deleteOrganization({ _id: _id })
+      .then(() => {
+        if (blogsList && blogsList.length > 0) {
+          const updatedList = blogsList.filter((result) => result._id !== _id);
+          setBlogsList(updatedList);
+        }
+        toast.success("Organization Succesfully Deleted", {
+          pauseOnHover: false,
+          autoClose: 2000,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.error("Problem Deleting Organization", {
+          autoClose: 2000,
+          pauseOnHover: false,
+        });
+      });
+  };
 
   const columns = [
     {
@@ -20,6 +59,23 @@ const ExtrernallyAddedOrgs = () => {
       dataIndex: "PhoneNumber",
       key: "PhoneNumber",
       width: "50%",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => {
+        return (
+          <Space size="middle">
+            <Button
+              type="danger"
+              onClick={() => openConfirmationModal(record?._id)}
+            >
+              Delete
+            </Button>
+          </Space>
+        );
+      },
+      hidden: role === "admin" ? false : true,
     },
   ].filter((column) => column.hidden !== true);
 
@@ -43,7 +99,20 @@ const ExtrernallyAddedOrgs = () => {
 
   return (
     <>
+      <ConfirmationModal
+        isVisible={isConfirmationModalVisible}
+        handleClose={() => {
+          setConfirmationModal(false);
+        }}
+        handleSubmit={() => {
+          if (companyToBeDeleted) {
+            setConfirmationModal(false);
+            deleteHandler(companyToBeDeleted);
+          }
+        }}
+      />
       <h2>Externally Added Orgs</h2>
+
       <Table columns={columns} dataSource={blogsList} />
     </>
   );
